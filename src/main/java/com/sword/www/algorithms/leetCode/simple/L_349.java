f1d446993a5db0d83e028476c3c13e5b9a6382ee
linmeng <1812323571@qq.com>
2020-08-04 11:26:57 +0800

    两个数组重复元素

---
+++ src/main/java/com/sword/www/algorithms/leetCode/simple/L_349.java
@@ -0,0 +1,44 @@
+package com.sword.www.algorithms.leetCode.simple;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 两个数组的交集
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ * @author linmeng
+ * @version 1.0
+ * @date 2020/8/4 10:34
+ */
+public class L_349 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        int[] intersection = intersection(nums1, nums2);
+        System.out.println();
+    }
+    /**
+     * 使用HashSet 对一个数组转set，使用set里面的contains方法求交集
+     * @param nums1
+     * @param nums2
+     * @author linmeng
+     * @date 2020/8/4 10:40
+     * @return int[]
+     */
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        int len;
+        if (nums1 == null || nums2 == null || (len = nums1.length) == 0 || nums2.length == 0){
+            return new int[0];
+        }
+        HashSet<Integer> set = new HashSet<>(len);
+        Arrays.stream(nums1).forEach(set::add);
+
+        return Arrays.stream(nums2).filter(set::contains).distinct().toArray();
+    }
+}
