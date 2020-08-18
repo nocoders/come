7d075a87c6ae6ac2034d20c419c18117e9911311
linmeng <1812323571@qq.com>
2020-08-18 10:56:42 +0800

    14查找字符串数组中的最长公共前缀

--- src/main/java/com/sword/www/algorithms/leetCode/simple/L_14.java
+++ src/main/java/com/sword/www/algorithms/leetCode/simple/L_14.java
@@ -1,7 +1,7 @@
 package com.sword.www.algorithms.leetCode.simple;
 
 /**
- * 查找字符串数组中的最长公共前缀
+ * 14查找字符串数组中的最长公共前缀
  *
  * @author linmeng
  * @version 1.0
@@ -15,8 +15,11 @@ public class L_14 {
         System.out.println("123".substring(0,0));
     }
     /**
-     * 使用 String.startWith()每个判断
-     *
+     * 使用 String.startWith()每个判断,
+     * 从首字符开始判断，全部成功，再加 一个字符
+     * @update https://leetcode-cn.com/problems/longest-common-prefix/solution/zui-chang-gong-gong-qian-zhui-by-leetcode-solution/
+     *      看完题解 ，感觉自己代码水平确实差点事，同样思路，我的代码 复杂很多
+     *      这个是判断第一个数组每个字符是否同其他数组相同，不相同直接返回。
      * @param strs 传入字符串数组
      * @author linmeng
      * @date 2020/7/2 21:28
@@ -31,27 +34,17 @@ public class L_14 {
         }
         int length = strs.length;
         int strLength = strs[0].length();
-        // 前缀
-        String prefix = strs[0].substring(0,1);
-        boolean isTrue = true;
         // 遍历所有的字符串
         for (int i = 0 ; i < strLength; i++) {
+            char c = strs[0].charAt(0);
             for (int j = 1; j < length; j++) {
-                if (!strs[j].startsWith(prefix)){
-                    isTrue = false;
-                    break;
+                if (i == strs[j].length() || strs[j].charAt(i) != c){
+                    return strs[0].substring(0,i);
                 }
-            }
-            if (isTrue){
-                if (i<strLength-1){
-                    prefix = prefix + strs[0].charAt(i+1);
-                }else {
-                    return prefix;
-                }
-            }else {
-                return prefix.substring(0,prefix.length()-1);
             }
         }
-        return prefix;
+        return  strs[0];
     }
+
+
 }
