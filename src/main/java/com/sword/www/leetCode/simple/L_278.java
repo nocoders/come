9db4356c27222200157c28ba94930a353d927ad7
linmeng <1812323571@qq.com>
2020-10-29 17:13:26 +0800

    278. 第一个错误的版本

--- src/main/java/com/sword/www/leetCode/simple/L_278.java
+++ src/main/java/com/sword/www/leetCode/simple/L_278.java
@@ -17,10 +17,28 @@ import com.sword.www.leetCode.bean.VersionControl;
  * @version 1.0
  * @date 22/10/2020 下午4:10
  */
-public class L_278 implements VersionControl {
-    private static boolean[] versionRes = {true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false};
-    @Override
-    public boolean isBadVersion(int version) {
-        return false;
+public class L_278{
+    private static final int firstBadVersion = 2;
+    public static void main(String[] args) {
+        int i = firstBadVersion(3);
+        System.out.println();
+    }
+    public static int firstBadVersion(int n) {
+        int left = 1,right = n;
+        while (left<right){
+            int mid = left +(right-left)/2;
+            if (isBadVersion(mid)){
+                right = mid;
+            }else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+    public static boolean isBadVersion(int version) {
+        if (version<firstBadVersion){
+            return false;
+        }
+        return true;
     }
 }
