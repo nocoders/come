3949a97674caee0606cdeb604112931567fcfc33
林猛 <1812323571@qq.com>
2020-12-12 17:28:33 +0800

    707. 设计链表  TODO  有问题，看不出来

---
+++ src/main/java/com/sword/www/leetCode/medium/L_707.java
@@ -0,0 +1,138 @@
+package com.sword.www.leetCode.medium;
+
+/**
+ * 707. 设计链表
+ * 设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
+ * 在链表类中实现这些功能：
+ * get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+ * addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+ * addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+ * addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+ * deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+ * 示例：
+ * MyLinkedList linkedList = new MyLinkedList();
+ * linkedList.addAtHead(1);
+ * linkedList.addAtTail(3);
+ * linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+ * linkedList.get(1);            //返回2
+ * linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+ * linkedList.get(1);            //返回3
+ * 提示：
+ * 所有val值都在 [1, 1000] 之内。
+ * 操作次数将在  [1, 1000] 之内。
+ * 请不要使用内置的 LinkedList 库。
+ * @author linmeng
+ * @version 1.0
+ * @date 2020年12月12日 16:25
+ */
+public class L_707 {
+    public static void main(String[] args) {
+//        MyLinkedList linkedList = new MyLinkedList();
+//        linkedList.addAtHead(1);
+//        linkedList.addAtTail(3);
+//        linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+//        linkedList.get(1);            //返回2
+//        linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+//        linkedList.get(1);            //返回3
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.addAtHead(2);
+        linkedList.deleteAtIndex(1);
+        linkedList.addAtHead(2);
+        linkedList.addAtHead(7);
+        linkedList.addAtHead(3);
+        linkedList.addAtHead(2);
+        linkedList.addAtHead(5);
+        linkedList.addAtTail(5);
+        linkedList.get(5);
+        linkedList.deleteAtIndex(6);
+        linkedList.deleteAtIndex(4);
+//        //链表变为1-> 2-> 3
+//        linkedList.addAtIndex(1,2);
+//        //返回2
+//        linkedList.get(1);
+//        //现在链表是1-> 3
+//        linkedList.deleteAtIndex(1);
+//        //返回3
+//        linkedList.get(1);
+        System.out.println();
+    }
+    static class MyLinkedList {
+        int size;
+        ListNode head;
+        /** Initialize your data structure here. */
+        public MyLinkedList() {
+            size=0;
+            head = new ListNode(0);
+        }
+
+        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        public int get(int index) {
+            if (index<0 || index>=size){
+                return -1;
+            }
+            ListNode curr = head;
+            for (int i = 0; i < index+1; i++) {
+                curr = curr.next;
+            }
+            return curr.val;
+        }
+
+        /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+        public void addAtHead(int val) {
+            addAtIndex(0,val);
+        }
+
+        /** Append a node of value val to the last element of the linked list. */
+        public void addAtTail(int val) {
+            addAtIndex(size,val);
+        }
+
+        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        public void addAtIndex(int index, int val) {
+            if (index>size){
+                return;
+            }
+            if (index<0){
+                index=0;
+            }
+            ListNode pred = head;
+            for (int i = 0; i < index; ++i) {
+                pred = pred.next;
+            }
+            ListNode addNode = new ListNode(val);
+
+            addNode.next = pred.next;
+            pred.next = addNode;
+            ++size;
+        }
+
+        /** Delete the index-th node in the linked list, if the index is valid. */
+        public void deleteAtIndex(int index) {
+            if (index<0 || index>size){
+                return;
+            }
+            ListNode pred = head;
+            for (int i = 0; i < index; i++) {
+                pred = pred.next;
+            }
+            pred.next = pred.next.next;
+            --size;
+        }
+    }
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
+}
