7a209c91068dd9ad76319aec0c2c1944503016a9
林猛 <1812323571@qq.com>
2021-01-15 10:46:09 +0800

    102. 二叉树的层序遍历 复习

--- src/main/java/com/sword/www/leetCode/medium/L_102.java
+++ src/main/java/com/sword/www/leetCode/medium/L_102.java
@@ -2,11 +2,10 @@ package com.sword.www.leetCode.medium;
 
 import com.sword.www.leetCode.bean.TreeNode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
+ * 102. 二叉树的层序遍历
  * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
  *
  * 示例：
@@ -34,14 +33,47 @@ public class L_102 {
     public static void main(String[] args) {
         Integer[] a = {3,9,20,null,null,15,7};
         TreeNode treeNode = TreeNode.arrayTransferToNode(a,0);
-        List<List<Integer>> lists = levelOrder(treeNode);
+        List<List<Integer>> lists = levelOrder2(treeNode);
         System.out.println();
     }
+    public static List<List<Integer>> levelOrder2(TreeNode root) {
+        Queue<TreeNode>queue = new LinkedList<>();
+        queue.add(root);
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        while (!queue.isEmpty()){
+            ArrayList<Integer> levelInfos = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                levelInfos.add(node.val);
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(levelInfos);
+        }
+
+        return res;
+    }
+    /**
+     * 每一层的数据的值保存到一个集合里面，然后保存下一层的节点，递归执行这一流程
+     * @param root
+     * @author linmeng
+     * @date 2021年1月15日 09:55
+     * @return java.util.List<java.util.List<java.lang.Integer>>
+     */
     public static List<List<Integer>> levelOrder(TreeNode root) {
         ArrayList<List<Integer>> res = new ArrayList<>();
         if (root == null){
             return res;
         }
+        // 将根节点下面的左右节点放入集合中
         res.add(Arrays.asList(root.val));
         ArrayList<TreeNode> param = new ArrayList<>();
         if (root.left!=null){
@@ -59,7 +91,9 @@ public class L_102 {
         if (param == null || param.size()==0){
             return;
         }
+        // 用于递归保存下一层的节点
         ArrayList<TreeNode> nextParam = new ArrayList<>();
+        // 保存当前层的节点的值
         ArrayList<Integer> currentVal = new ArrayList<>();
         for (TreeNode node : param) {
            currentVal.add(node.val);
