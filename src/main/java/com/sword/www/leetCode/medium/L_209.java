89abc1750a7037fd2fbbf872beb7156bb6191f91
林猛 <1812323571@qq.com>
2021-03-28 11:17:35 +0800

    209. 长度最小的子数组 复习

--- src/main/java/com/sword/www/leetCode/medium/L_209.java
+++ src/main/java/com/sword/www/leetCode/medium/L_209.java
@@ -1,7 +1,7 @@
 package com.sword.www.leetCode.medium;
 
 /**
- *   长度最小的子数组
+ *   209. 长度最小的子数组
  *  给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。
  * 如果不存在符合条件的子数组，返回 0。
  * @author linmeng
@@ -12,7 +12,7 @@ public class L_209 {
 
     public static void main(String[] args) {
         int [] nums = {2,3,1,2,4,3};
-        int i = minSubArrayLen(7, nums);
+        int i = minSubArrayLen1(7, nums);
         System.out.println();
     }
     /**
@@ -41,5 +41,37 @@ public class L_209 {
             }
         }
         return res == Integer.MAX_VALUE?0:res;
+    }
+
+    /**
+     * 寻找数组中长度最短的大于target的连续数组
+     *  判断数组是否为空，
+     *  定义两个指针i=j=0，一个头一个尾，定义sum作为同target判断的元素
+     *  sum加j角标下的值。
+     *  判断sum是否大于target，大于的话，res=max[res,j-i+1],循环将 sum-nums[i++]
+     *  j++
+     * @param target
+     * @param nums
+     * @author linmeng
+     * @date 2021年3月28日 10:05
+     * @return int
+     */
+    public static int minSubArrayLen1(int target, int[] nums) {
+        int res = Integer.MAX_VALUE,length,i=0,j=i,sum = 0;
+        if (nums==null || (length = nums.length)<1){
+            return 0;
+        }
+        while (j<length){
+            if (nums[i]>=target){
+                return 1;
+            }
+            sum+=nums[j];
+            while (sum>=target){
+                res = Math.min(j-i+1,res);
+                sum = sum-nums[i++];
+            }
+            j++;
+        }
+        return res==Integer.MAX_VALUE?0:res;
     }
 }
