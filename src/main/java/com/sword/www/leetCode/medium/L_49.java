32d262ac3f37b0466a582328aa0cf91215a1365b
林猛 <1812323571@qq.com>
2021-03-28 22:31:01 +0800

    49. 字母异位词分组

--- src/main/java/com/sword/www/leetCode/medium/L_49.java
+++ src/main/java/com/sword/www/leetCode/medium/L_49.java
@@ -1,9 +1,6 @@
 package com.sword.www.leetCode.medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * 49. 字母异位词分组
@@ -28,11 +25,14 @@ public class L_49 {
 
     public static void main(String[] args) {
         String [] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
-        List<List<String>> lists = groupAnagrams(strs);
+        List<List<String>> lists = groupAnagrams2(strs);
+//        StringBuilder stringBuilder = new StringBuilder();
+
         System.out.println();
     }
     /**
      * 理解：将字符串数组中的字符串按照不排序的情况下字符串中的相等进行分组
+     *      使用排序后的字符串为key，集合保存相同字符串数组
      * @param strs
      * @author linmeng
      * @date 2021年1月5日 17:19
@@ -51,4 +51,31 @@ public class L_49 {
 
         return new ArrayList<>(map.values()) ;
     }
+    /**
+     * 遍历每个字符串，将字符串每个字符出现的次数保存到数组中，然后字符跟次数组装成字符串，作为key，value是字符串本身。
+     *
+     * @param strs
+     * @author linmeng
+     * @date 2021年3月28日 22:04
+     * @return java.util.List<java.util.List<java.lang.String>>
+     */
+    public static List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int length = str.length();
+            int[] nums = new int[26];
+            for (int i = 0; i < length; i++) {
+                nums[str.charAt(i)-'a']=nums[str.charAt(i)-'a']+1;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nums.length; i++) {
+                sb.append(i+'a').append(nums[i]);
+            }
+            List<String> list = map.getOrDefault(sb.toString(), new ArrayList<>());
+            list.add(str);
+            map.put(sb.toString(),list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
 }
