a33493b078c1de882cda0a1098a8ddc96db2727f
林猛 <1812323571@qq.com>
2021-03-29 21:25:09 +0800

    5. 最长回文子串 复习

--- src/main/java/com/sword/www/leetCode/medium/L_5.java
+++ src/main/java/com/sword/www/leetCode/medium/L_5.java
@@ -1,6 +1,7 @@
 package com.sword.www.leetCode.medium;
 
 /**
+ * 5. 最长回文子串
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  * @author linmeng
  * @version 1.0
@@ -60,4 +61,64 @@ public class L_5 {
         //  这个地方判断不通过的时候，已经自减自加完成了，减一的原因我也不清楚，但是人家代码没问题，很烦。
         return right-left-1;
     }
+
+    /**
+     * 求最长回文字符串，从两边往中心走
+     * ERROR 两边往中间走是从哪边开始走，这个很麻烦，还是应该从中间往两边走，就无所谓哪一边了
+     * @param s
+     * @author linmeng
+     * @date 2021年3月29日 20:38
+     * @return java.lang.String
+     */
+    public static String longestPalindrome2(String s) {
+        int res=0,length = s.length();
+        String reStr="";
+        for (int i = 0; i < length-res; i++) {
+            int k=i,j=length-1;
+            while (k<j&&s.charAt(k)==s.charAt(j)){
+                k++;
+                j--;
+            }
+            if (k>=j && res < length-i){
+                res = length-i;
+                reStr = s.substring(i,length);
+            }
+        }
+
+        return reStr;
+    }
+    /**
+     *
+     * 复习，从中间往两边扩散
+     * @param s
+     * @author linmeng
+     * @date 2021年3月29日 21:00
+     * @return java.lang.String
+     */
+    public static String longestPalindrome3(String s) {
+        int length = s.length(),beginIndex=0,endIndex=0;
+        String resStr="";
+        for (int i = 0; i < length - (resStr.length() + 1) / 2; i++) {
+            int len1 = getPalindrome(s,i,i);
+            int len2 = getPalindrome(s,i,i+1);
+            int len = Math.max(len1,len2);
+            if (len>endIndex-beginIndex){
+                // 奇数个的话减一同不减一相同，偶数个的话因为后面有加一，所以不用减
+                beginIndex = i-(len-1)/2;
+                endIndex = i+len/2;
+            }
+        }
+
+        return  s.substring(beginIndex,endIndex+1);
+    }
+
+    private static int getPalindrome(String s,int i, int j) {
+        int length = s.length();
+        while (i>=0 && j<length && s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
+        }
+        return j-i+1;
+    }
+
 }
