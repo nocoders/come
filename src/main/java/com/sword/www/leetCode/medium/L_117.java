e7e941f98b209ff6908560a1c77c912ab1157b7e
林猛 <1812323571@qq.com>
2021-04-07 21:28:39 +0800

    117. 填充每个节点的下一个右侧节点指针 II 复习

--- src/main/java/com/sword/www/leetCode/medium/L_117.java
+++ src/main/java/com/sword/www/leetCode/medium/L_117.java
@@ -1,8 +1,26 @@
 package com.sword.www.leetCode.medium;
 
 /**
- * TODO 不会，看不懂
- *
+ * 117. 填充每个节点的下一个右侧节点指针 II
+ * 给定一个二叉树
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 进阶：
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * 示例：
+ * 输入：root = [1,2,3,4,5,null,7]
+ * 输出：[1,#,2,3,#,4,5,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化输出按层序遍历顺序（由 next 指针连接），'#' 表示每层的末尾。
+ * 提示：
+ * 树中的节点数小于 6000
+ * -100 <= node.val <= 100
  * @author linmeng
  * @version 1.0
  * @date 2021年2月2日 11:08
@@ -58,7 +76,9 @@ public class L_117 {
         }
         Node start = root;
         while (start != null) {
+            // 保存上一个节点
             last = null;
+            // 用于当前层遍历完成后，保存下一行元素的起点，复制给start
             nextStart = null;
             for (Node p = start; p != null; p = p.next) {
                 if (p.left != null) {
@@ -73,6 +93,7 @@ public class L_117 {
         return root;
     }
 
+
     public static void handle(Node p) {
         if (last != null) {
             last.next = p;
@@ -81,5 +102,46 @@ public class L_117 {
             nextStart = p;
         }
         last = p;// 把p节点赋值给last
+    }
+
+    /**
+     * 复习：从根节点开始，每层都是已经有next指针的。
+     *      定义三个变量：start：每层的开始节点，用于遍历，nextStart：下一层的开始节点，
+     * 遍历start层时将start的第一个节点的不为空的节点赋值。last：保存该层上一个节点。
+     *
+     * @param root
+     * @author linmeng
+     * @date 2021年4月7日 21:17
+     * @return com.sword.www.leetCode.medium.L_117.Node
+     */
+    public static Node connectFx(Node root) {
+        if (root==null){
+            return null;
+        }
+        Node start=root;
+        while (start!=null){
+            last=null;
+            nextStart=null;
+            for(Node p=start;p!=null;p=p.next){
+                if (p.left!=null){
+                    handleFx(p);
+                }
+                if (p.right!=null){
+                    handleFx(p);
+                }
+            }
+            start=nextStart;
+        }
+        return root;
+    }
+
+    private static void handleFx(Node p) {
+        if (nextStart==null){
+            nextStart=p;
+        }
+        if (last!=null){
+            last.next=p;
+        }
+        last=p;
     }
 }
