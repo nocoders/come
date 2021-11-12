03c27c4cdc31a445e528471a681123c9a498d4aa
linmeng <1812323571@qq.com>
2021-11-12 14:13:27 +0800

    netty  init

---
+++ src/main/java/com/sword/www/netty/HelloNettyServer.java
@@ -0,0 +1,20 @@
+package com.sword.www.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class HelloNettyServer {
+    public static void main(String[] args) {
+        // 线程组
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,128);
+        }catch (Exception e){}
+    }
+}
